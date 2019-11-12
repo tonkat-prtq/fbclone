f7056b3107bd01bf31edacc553380e3fa45309f8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-12 10:54:24 +0900

    記述ミス修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) # ストロングパラメータ設定
     if @user.save
-      
+
     else
       render 'new'
+    end
   end
 
   private
