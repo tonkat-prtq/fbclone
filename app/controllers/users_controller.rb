bfa9e2fd7a6cf2dfdf39390b4b1f4eba0c6b8dbe
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-12 10:49:43 +0900

    strong parameter追加、createアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params) # ストロングパラメータ設定
+  end
+
+  private
+
+  def user_params
+    param.require(:user).permit(:name, :email, :password, :password_confirmation)
+    # userキーのpermit以下のパラメータのみ受け取るという意味
+  end
+
 end
