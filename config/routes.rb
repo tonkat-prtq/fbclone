668938e006f1dcfb26d0c15df34b66da89c8e493
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-14 21:33:30 +0900

    resources :blog, migration CreateBlogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
+  resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
