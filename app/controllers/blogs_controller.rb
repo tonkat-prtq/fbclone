28f50386f7ad76218509dac8e5151fc7cfbee107
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-14 22:06:54 +0900

    createアクションにブログ作成メソッドを実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,4 +5,9 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
+  def create
+    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+  end
+
 end
