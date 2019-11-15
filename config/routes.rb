f3912ad406d118b89dc7030bcf7b3f03301a7d6f
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-15 11:36:17 +0900

    編集→投稿時も確認画面を表示されるようチェレンジ

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     collection do
       post :confirm
     end
+    member do
+      patch :confirm
+    end
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
