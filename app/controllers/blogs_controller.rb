2c80e45a3e0d2604c41bb008d5267b14bdf74327
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-15 11:21:29 +0900

    confirmアクション追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -36,6 +36,10 @@ class BlogsController < ApplicationController
     redirect_to blogs_path, notice: "ブログを削除しました"
   end
 
+  def confirm
+    @blog = Blog.new(blog_params)
+  end
+
   private
 
   def blog_params
