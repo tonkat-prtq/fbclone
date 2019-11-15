aca4d1d1e187d85f64fe1b9afb2eaa6035b505ec
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-15 11:31:40 +0900

    戻るボタンを押したときに情報が保持されたまま戻れるように

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,10 +10,14 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path, notice: "ブログを作成しました！"
-    else
+    if param[:back]
       render 'new'
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました"
+      else
+        render 'new'
+      end
     end
   end
 
