f18c9b636d21300545a3c403f198ef84c2eeada8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-18 22:37:15 +0900

    favorite_usersメソッドを実装

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
 end
