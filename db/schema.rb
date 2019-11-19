28eca95d2065fad91c1984d029b85a757dbc215e
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-19 09:59:47 +0900

    userとfeedsの関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_002632) do
+ActiveRecord::Schema.define(version: 2019_11_19_005847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_11_19_002632) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema.define(version: 2019_11_19_002632) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "feeds", "users"
 end
