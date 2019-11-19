28d354aabe39e374eda3d7d390a65c84f06657fc
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-11-19 09:48:44 +0900

    写真アップロード機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_140029) do
+ActiveRecord::Schema.define(version: 2019_11_19_002632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2019_11_18_140029) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
